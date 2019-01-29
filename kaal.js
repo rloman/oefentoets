610e95734a25db23b162b3650fabd8b9e46c6744
Raymond Loman <raymond@carpago.nl>
2019-01-29 18:17:35 +0100

    Funny ... have a working case with a Kaartenbak now in kaal.js (name is changebla)

--- kaal.js
+++ kaal.js
@@ -23,7 +23,42 @@ class Kaartenbak {
         });
     }
 
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+    insert(tocht) {
+        this.connection.query("insert into tocht set ?", [tocht], (err, result) => {
+                console.log(result.insertId);
+        })
+    }
+
 }
+
+// main
 
 let kaartenBak = new Kaartenbak();
 
+kaartenBak.query("select * from tocht").then(rows => {
+    for(let row of rows) {
+        console.log(row.id+", "+row.start);
+    }
+});
+
+let tocht = {
+    start: new Date(),
+    end: new Date()
+}
+kaartenBak.insert(tocht);
+
+kaartenBak.query("select * from tocht").then(rows => {
+    for(let row of rows) {
+        console.log(row.id+", "+row.start);
+    }
+});
