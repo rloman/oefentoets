86e2263370254fcaae529bc8ff7f6b3df7674978
Raymond Loman <raymond@carpago.nl>
2019-06-24 11:28:23 +0200

    Some assert to validate myself

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 'use strict';
 
+
+
 let Tocht = require('./modules/tocht');
 let kaartenBak = require('./modules/kaartenbak');
 
+function assert(b, message) {
+    if(!b) {
+        console.error(message);
+        throw new Error(message);
+    }
+}
+
 console.log("Starting main ... ");
+
+
 
 // clear all (for demo)
 kaartenBak.removeAll().then(result => {
 
+    assert(result, "Result should be truthy")
     if(result) {
         console.log(`Removed all tochten`);
     }
@@ -21,48 +33,67 @@ let tocht = {
 };
 
 let createdTochtPromise = kaartenBak.insert(tocht);
+assert(createdTochtPromise.then);
 
 
 // be aware that here createdTocht and error are parameters of arrow functions
 createdTochtPromise.then(createdTocht => {
     console.log("Created tocht: >" + createdTocht.id + "<");
+    assert(0 !== tocht.id, "The id of tocht should not be zero");
 }, error => {
     console.log("Error: >" + error + "<");
 });
 
 
 kaartenBak.query('insert into tocht set ?', [tocht]).then(result => {
+    assert(true);
     console.log("Inserted tocht with id:" + result.insertId);
 }, error => {
+    assert(false);
     console.log("Error: " + error);
 });
 
 
 kaartenBak.insertAlternate(new Date()).then(function (createdTocht) {
+    assert(true);
     console.log(createdTocht.id);
 }, function (error) {
+    assert(false);
     console.log("Foutje bedankt:" + error);
 })
 
 kaartenBak.createTocht(new Date()).then(function (tocht) {
+    assert(0 !== tocht.id);
     console.log("Created a tocht with id:" + tocht.id);
 }, function (error) {
+    asssert(false);
     console.log(error);
 });
 
 let promise = kaartenBak.getTochten();
 
+assert(promise.then, "Promise (promise) should be promise");
+
 promise.then(function (rows) {
+    assert(true);
     for (let row of rows) {
         console.log(row.id + ", " + row.start);
     }
-}, error => console.log(error));
+}, error =>  {
+    assert(false);
+    console.log(error);
+});
 
 // again be aware ... after then there follow TWO functions
 kaartenBak.getTochten().then(rows => {
+  
+    let counter = 0;
     for (let tocht of rows) {
+        assert(true);
+        counter++;
         console.log(tocht.id + ", " + tocht.start + ", " + tocht.end);
     }
+    assert(counter > 0);
 }, error => {
     console.log(error);
 });
