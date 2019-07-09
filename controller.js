adc8917b127d4622661a816f0b0f72c9a128bbb8
Raymond Loman <raymond@carpago.nl>
2019-07-09 13:55:31 +0200

    Add find one in controller (after adding findall already)

--- controller.js
+++ controller.js
@@ -35,9 +35,30 @@ app.get('/api/trips', async function (req, res) {
   res.end(JSON.stringify(users));
 });
 
+// this is to enable http://localhost:8081/api/users/3 or something
+// look that the 'id' is read out below
+app.get('/api/trips/:id', async function (req, res) {
+
+  let id = +req.params.id
+
+  let trip = await service.findById(id);
+  // OK we found a user
+  if (trip) {
+    res.setHeader('Content-Type', 'application/json')
+    // response end with a string of the found user
+    res.end(JSON.stringify(trip));
+  } else {
+    // error, we did NOT find a user
+    res.setHeader('Content-Type', 'application/json')
+
+    // so render the common 404 (Not found)
+    res.status(404).end();
+  }
+});
+
 // and finally ... run it :-)
 // get the server from the app which runs on port 8081
-let server = app.listen(8081, function() {
+let server = app.listen(8081, function () {
 
   console.log("Example app listening at http://%s:%s", server.address().address, server.address().port)
 });
